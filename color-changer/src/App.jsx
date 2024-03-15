631accec6c0172bae8b1c7e47dd893fb7859248b
surya <swainsuryakanta97@gmail.com>
2024-03-15 22:45:35 +0530

    small changes

--- color-changer/src/App.jsx
+++ color-changer/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
             <main className='w-[100vw] h-screen bg-black flex flex-col justify-end' style={{backgroundColor:color}}>
                 <div className="allbtns w-fit flex flex-row gap-4  bg-white rounded-full mx-auto py-2 px-6">
                     {btns.map(item=>{
-                        return <button   onClick={handleColorchange} className="rounded-md px-4 py-2 text-white font-bold" style={{backgroundColor: item.toLowerCase()}}>{item}</button>
+                        return <button onClick={handleColorchange} className="rounded-md px-4 py-2 text-white font-bold outline-none" style={{backgroundColor: item.toLowerCase()}}>{item}</button>
                     })}
                 </div>
             </main>
